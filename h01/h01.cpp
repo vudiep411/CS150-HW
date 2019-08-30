1c4ae12256475cc50b6b34095b944ede91384982
Vu Diep <vdiep8@student.cccd.edu>
2019-08-30 18:10:33 +0000

    auto commit 08/30/19 18:10:33

--- h01/h01.cpp
+++ h01/h01.cpp
@@ -8,12 +8,12 @@
 #include <iomanip>
 using namespace std;
 
-string STUDENT = "Who Are You?";  // Add your Canvas login name
+string STUDENT = "vdiep8";  // Add your Canvas login name
 extern string ASSIGNMENT;
 
 
 /**
- * One line describing what this program does.
+ * Calculate the time
  * @return 0 for success.
  */
 int run()
@@ -21,6 +21,25 @@ int run()
     // Add your implementation comments here
 
     // Write your code here
+char dis;
+int timeHours , timeMinutes , durationHours , durationMins;
+cout << "     Time: ";
+cin >> timeHours >> dis >> timeMinutes;
+cout << "     Duration: ";
+cin >> durationHours >> dis >> durationMins;
+int time =  timeHours * 60 + timeMinutes;
+int durAfter = durationHours * 60 + durationMins;
+int sumHours, sumMinutes, diffHours , diffMinutes;
+sumHours = (time + durationHours) / 60;
+sumMinutes = (time + durationMins) % 60;
+diffHours = (time - durationHours) / 60;
+diffMinutes = (time - durationHours) % 60;
+cout << endl;
+cout << durationHours << ":" << durationMins << " hours after, and before " << timeHours << ":" << timeMinutes << " is [" << sumHours << ":" << sumMinutes << ", " << diffHours << ":" << diffMinutes << endl;
+cout << setfill('0');
+cout << setw(2) << sumHours << ":" << setw(2) << sumMinutes ;
 
-    return 0;
+
+return 0;
+
 }
