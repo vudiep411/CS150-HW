7b326314a35924fe367772ddb59b21d545d47f17
Vu Diep <vdiep8@student.cccd.edu>
2019-08-31 01:07:52 +0000

    auto commit 08/31/19 01:07:52

--- h01/h01.cpp
+++ h01/h01.cpp
@@ -39,7 +39,7 @@ int beforehours = before / 60;
 int beforeminutes = before % 60;
 
 //output
-cout << endl;
+
 cout << setfill('0'); // only needed once
 cout << setw(2) << after / 60 << ":" << setw(2) << after % 60;
 cout << durationHours << ":" << durationMins << " hours after, and before, " << timeHours << ":" << timeMinutes << " is [" << afterhours << ":" << afterminutes << ", " << beforehours << ":" << beforeminutes << "]" << endl;
