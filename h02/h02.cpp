f252539ba2f487c1167a70b8999d3ce731b7da0e
Vu Diep <vdiep8@student.cccd.edu>
2019-09-02 19:04:54 +0000

    auto commit 09/02/19 19:04:54

--- h02/h02.cpp
+++ h02/h02.cpp
@@ -19,20 +19,21 @@ extern string ASSIGNMENT;
 int run()
 {
 //input
-float amountNeeded , mouseWeight , weightDesire , lethalDose ;
+int amountNeeded , mouseWeight , weightDesire , lethalDose ;
+double cans;
 const int POUND_TO_GRAM = 454;
-
+const double PERCENTAGE_IN_GRAM = 7 / 2 ;
 cin >> mouseWeight ;
 cin >> amountNeeded ;
 cin >> weightDesire ;
 weightDesire = weightDesire * POUND_TO_GRAM ;
 lethalDose = (weightDesire * amountNeeded) / mouseWeight;
-
+cans = lethalDose / PERCENTAGE_IN_GRAM ;
 //output
 cout << "Weight of the mouse in grams: " << mouseWeight << endl;
 cout << "Lethal dose for the mouse (in grams): " << lethalDose << endl;
 cout << "Desired weight of the dieter (in pounds): " << weightDesire << endl;
-cout << "Lethal dose in grams, cans is [" << setprecision(0) << lethalDose << ", " << 864762 << "]" << endl;
+cout << "Lethal dose in grams, cans is [" << setprecision(0) << lethalDose << ", " << cans << "]" << endl;
 
 
     return 0;
