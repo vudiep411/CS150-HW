96fbabcc7f271f9170cdf9bd2acbfe1fdfc42997
Vu Diep <vdiep8@student.cccd.edu>
2019-09-11 05:12:51 +0000

    auto commit 09/11/19 05:12:51

--- h05/h05.cpp
+++ h05/h05.cpp
@@ -13,10 +13,13 @@ string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
 
 // Place the implementation (definition) of your function here
 
-double focalLength(double, double, double, double)
+double focalLength(double d, double r1, double r2, double n)
 {
-    int result = 0;
-    return result;
+    int f = (1 / r1) + (1 / r2);
+    int a = ((n - 1) * d) / (n * r1 * r2);
+    f = (n - 1) * (f + a);
+    f = 1 / f;
+    return f;
 
 
 }
