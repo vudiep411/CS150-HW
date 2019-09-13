6ba402f901758d5ba3256c79e7fc09232f64dcce
Vu Diep <vdiep8@student.cccd.edu>
2019-09-13 22:57:34 +0000

    auto commit 09/13/19 22:57:34

--- h06/h06.cpp
+++ h06/h06.cpp
@@ -3,16 +3,24 @@
  *  @date Put the date here
  *  @file h06.cpp
  */
+#include <iostream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
-string STUDENT = "WHO AM I?";  // Add your Canvas login name
+string STUDENT = "vdiep8";  // Add your Canvas login name
 
 // Implement your function here
+string toFrenchGender(const string& country)
+{
+    string result = country;
 
+    return result;
+}
 
 
 /////////////// Optional Student Code /////////////////
+
 int run()
 {
     cout << "Student testing" << endl;
