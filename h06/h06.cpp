426421ae4113c82db78804423638d495208ebbe3
Vu Diep <vdiep8@student.cccd.edu>
2019-09-14 00:18:24 +0000

    auto commit 09/14/19 00:18:24

--- h06/h06.cpp
+++ h06/h06.cpp
@@ -13,11 +13,16 @@ string STUDENT = "vdiep8";  // Add your Canvas login name
 // Implement your function here
 string toFrenchGender(const string& country)
 {
-
+    string vowels = "AEIOU";
+    auto first = vowels.find(country[0]);
     string island = "iles";
     auto len = country.size() - 1;
     string result;
     string last = country.substr(country.size() - 2);
+    if(first)
+    {
+        result = "l'" + country;
+    }
 
     if(country.substr(0 , 4).compare(island) == 0 || (last == "es" ||last == "is" || last == "os" || last == "as") )
     {
