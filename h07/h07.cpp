87491d437f0bc00c94939703bd8f2dbd5f410e1d
Vu Diep <vdiep8@student.cccd.edu>
2019-09-19 05:17:54 +0000

    auto commit 09/19/19 05:17:54

--- h07/h07.cpp
+++ h07/h07.cpp
@@ -7,11 +7,34 @@
 #include <cctype>
 using namespace std;
 
-string STUDENT = "WHO AM I?";  // Add your Canvas login name
+string STUDENT = "vdiep8";  // Add your Canvas login name
 
 // Implement your function here
+int sumNums(const string& s)
+{
+    int sum = 0;
+    int num = 0;
+    for(size_t i = 0, len = s.size(); i <= len - 1; i++)
+    {
+        char a = s.at(i);
+        if(isdigit(a))
+        {
+         a -= '0';
+         num *= 10;
+         num += a;
+        }
+    else
+    {
+        sum += num;
+        num = 0;
+    }
 
 
+    }
+
+ return sum;
+
+}
 
 /////////////// Optional Student Code /////////////////
 int run()
