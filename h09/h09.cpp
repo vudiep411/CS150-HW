ba18b48f3d8d902c97823f3eceeb69dfa692cff2
Vu Diep <vdiep8@student.cccd.edu>
2019-09-22 06:22:37 +0000

    auto commit 09/22/19 06:22:37

--- h09/h09.cpp
+++ h09/h09.cpp
@@ -11,9 +11,20 @@ string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
 #include "h09.h"
 
 // Put your function implementation (definitions) in this file
+string codeForDigit(int digit)
+{
+    return ":::||";
+}
 
+int checkDigit(int zip)
+{
+    return 0;
+}
 
-
+string barCode(int zip)
+{
+    return ":::||";
+}
 
 /////////// Student Testing ///////////////////////
 int run()
