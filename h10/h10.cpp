e80822246b24e1b9e30b8c3f273f201b4690283a
Vu Diep <vdiep8@student.cccd.edu>
2019-09-26 06:18:31 +0000

    auto commit 09/26/19 06:18:31

--- h10/h10.cpp
+++ h10/h10.cpp
@@ -6,42 +6,89 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include "h10.h"
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "vdiep8"; // Add your Canvas/occ-email ID
 
 #include "h10.h"
 
 // Place your function definitions in this file.
+bool read(const string& agePrompt , int& age , bool check )
+{
+ // cout << agePrompt << endl;
+  cin >> age;
+  if(cin.fail())
+  {
+      cin.clear();
+      check = false;
+  }
+  else
+  {
+      check = true;
+  }
+  return check;
+}
 
+bool read(const string& gpaPrompt , double& gpa , bool check )
+{
+   //cout << gpaPrompt << endl;
+  cin >> gpa;
+  if(cin.fail())
+  {
+      cin.clear();
+      check = false;
+  }
+  else
+  {
+      cin.ignore(1024, '\n');
+      check = true;
+  }
+  return check;
+}
+
+bool read(const string& namePrompt , string& name , bool check )
+{
+ getline(cin , name);
+ return check;
+}
+
+bool read(char& ch , char dot)
+{
+ cin.get(ch);
+ if(ch == dot) return false;
+ return true;
+}
 ////////////////// STUDENT TESTING /////////////
 int run()
 {
     cout << "Student testing" << endl;
 
     // Uncomment the code below for informal testing
-    // int age;
-    // if (read("How old are you? ", age))
-    //     cout << "You don't look " << age << " years old!" << endl;
-    // else
-    //     cout << "Hmm. Doesn't look like you entered an int." << endl;
+     int age;
+     if (read("How old are you? ", age))
+        cout << "You don't look " << age << " years old!" << endl;
+     else
+         cout << "Hmm. Doesn't look like you entered an int." << endl;
 
-    // double gpa;
-    // if (read("What is your gpa? ", gpa, true))
-    //     cout << "Wow! " << fixed << setprecision(2) << gpa << "? I'm impressed." << endl;
-    // else
-    //     cout << "Sorry. I can't understand what you typed." << endl;
+     double gpa;
 
-    // string name;
-    // if (read("What is your full name? ", name, true))
-    //     cout << "Hi " << name << ". Glad to meet you." << endl;
-    // else
-    //     cout << "Hmm. Is that your FULL name?" << endl;
+     if (read("What is your gpa? ", gpa, true))
+         cout << "Wow! " << fixed << setprecision(2) << gpa << "? I'm impressed." << endl;
+     else
+         cout << "Sorry. I can't understand what you typed." << endl;
 
-    // cout << "Type a sentence ending in a period: ";
-    // char ch;
-    // while (read(ch, '.')) cout << ch;
-    // cout << "." << endl;
+     string name;
+     if (read("What is your full name? ", name, true))
+         cout << "Hi " << name << ". Glad to meet you." << endl;
+     else
+         cout << "Hmm. Is that your FULL name?" << endl;
+
+     cout << "Type a sentence ending in a period: ";
+     char ch;
+     while (read(ch, '.')) cout << ch;
+     cout << "." << endl;
+
 
     return 0;
 }
