a77f6420cd535c75ae4d927fedce57369e53521f
Vu Diep <vdiep8@student.cccd.edu>
2019-10-04 00:58:26 +0000

    auto commit 10/04/19 00:58:25

--- h13/h13.cpp
+++ h13/h13.cpp
@@ -10,12 +10,38 @@
 #include <iomanip>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "vdiep8"; // Add your Canvas/occ-email ID
 
 #include "h13.h"
 
 // Define your function here
+std::string dataSets(const std::string& fileName)
+{
+    ostringstream result;
+    ifstream in;
+    in.open(fileName.c_str());
+    if(!in.is_open()) {result << "data/notfound.txt cannot be opened.\n";}
 
+    int dataSet = 1;
+    int i;
+    while (in >> i)
+    {
+        int count = 0;
+        double sum = 0.0;
+        while (i != 0)
+        {
+            double value;
+            in >> value;
+            count += i;
+            sum += i*value;
+            in >> i;
+        }
+        if (dataSet > 1) result << "\n";
+        result << fixed << setprecision(4) << "data set " << dataSet << ": total values = " << count << "\naverage value = " << sum / static_cast<double>(count) << endl;
+        dataSet++;
+    }
+    return result.str();
+}
 
 ///////////////// Student Testing /////////////////////////
 int run()
