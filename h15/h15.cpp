91db9dff3881cbdf4b5e94d491388a8999307ebe
Vu Diep <vdiep8@student.cccd.edu>
2019-10-08 18:57:02 +0000

    auto commit 10/08/19 18:57:02

--- h15/h15.cpp
+++ h15/h15.cpp
@@ -6,7 +6,7 @@
 #include <string>
 using namespace std;
 
-string STUDENT = WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "vdiep8?"; // Add your Canvas/occ-email ID
 
 // NO OTHER CODE IN THIS FILE
 // PUT EVERYTHING IN THE HEADER FILE
