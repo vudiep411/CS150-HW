1984837f647338ac9c401844bbb861ef084d09c5
Vu Diep <vdiep8@student.cccd.edu>
2019-10-15 05:55:07 +0000

    auto commit 10/15/19 05:55:07

--- h17/h17.cpp
+++ h17/h17.cpp
@@ -7,13 +7,53 @@
 #include <cmath>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "vdiep8"; // Add your Canvas/occ-email ID
 
 #include "h17.h"
 
 // Write your functions here
+string toString(Suit s)
+{
+    switch(s)
+    {
+       case Suit::CLUBS: return "Clubs";
+       case Suit::SPADES: return "Spade";
+       case Suit::DIAMONDS: return "Diamonds";
+       case Suit::HEARTS: return "Hearts";
 
 
+
+    /*    ACE, TWO, THREE, FOUR, FIVE,
+    SIX, SEVEN, EIGHT, NINE, TEN,
+    JACK, QUEEN, KING */
+    }
+return "???";
+}
+string toString(Rank r)
+{
+    switch(r)
+    {
+       case Rank::ACE: return "Ace";
+       case Rank::TWO: return "2";
+       case Rank::THREE: return "3";
+       case Rank::FOUR: return "4";
+       case Rank::FIVE: return "5";
+       case Rank::SIX: return "6";
+       case Rank::SEVEN: return "7";
+       case Rank::EIGHT: return "8";
+       case Rank::NINE: return "9";
+       case Rank::TEN: return "10";
+       case Rank::JACK: return "Jack";
+       case Rank::QUEEN: return "Queen";
+       case Rank::KING: return "King";
+    }
+    return "???";
+}
+ostream& operator<<(std::ostream& out, const Card& c)
+{
+    out << toString(c.rank) << " of " << toString(c.suit);
+    return out;
+}
 
 /////////////// STUDENT TESTING ////////////////////
 #include <iostream>
