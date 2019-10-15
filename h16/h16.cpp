ab80c932054fa0d06957c58df1c3e29345416b47
Vu Diep <vdiep8@student.cccd.edu>
2019-10-15 05:11:49 +0000

    auto commit 10/15/19 05:11:49

--- h16/h16.cpp
+++ h16/h16.cpp
@@ -7,11 +7,35 @@
 #include <cmath>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "vdiep8"; // Add your Canvas/occ-email ID
 
 #include "h16.h"
 // Add your implementation here
-
+void get(istream& in, Point& p)
+{
+    in >> p.x >> p.y;
+}
+void print(ostream& out, const Point& p)
+{
+    out << "(" << p.x << ", " << p.y << ")";
+}
+double distanceBetween(const Point& a, const Point& b)
+{
+    double result = sqrt( (pow((a.x - b.x) , 2) ) + (pow((a.y - b.y) , 2)  ));
+    return result;
+}
+Point midpoint(const Point& a, const Point& b)
+{
+    Point mid;
+    mid.x = (a.x + b.x) / 2;
+    mid.y = (a.y + b.y) / 2;
+    return mid;
+}
+double perimeter(const Triangle& t)
+{
+    double result = distanceBetween(t.p1 , t.p2) + distanceBetween(t.p2 , t.p3) + distanceBetween(t.p1 , t.p3);
+    return result;
+}
 
 
 /////////////// STUDENT TESTING ////////////////////
