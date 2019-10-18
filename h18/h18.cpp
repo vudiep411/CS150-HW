8da522f433cf9f68bbc683e0db945e53b203043e
Vu Diep <vdiep8@student.cccd.edu>
2019-10-18 23:41:51 +0000

    auto commit 10/18/19 23:41:51

--- h18/h18.cpp
+++ h18/h18.cpp
@@ -9,15 +9,37 @@
 #include <stdexcept>
 #include <fstream>
 #include <cctype>
+#include <sstream>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "vdiep8"; // Add your Canvas/occ-email ID
 
 
 #include "h18.h"
 
 // Place your function definition here
+vector<string> fileToWords(const string& filename)
+{
+    ifstream in(filename);
+    if(in.fail()) throw invalid_argument("invalid file name.");
 
+    vector<string> results;
+    string line;
+    while(getline(in , line))
+    {
+       string word;
+       istringstream check(line);
+     while(check >> word)
+     {
+         results.push_back(word);
+     }
+
+    }
+    return results;
+
+
+
+}
 
 
 
