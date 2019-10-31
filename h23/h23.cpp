b82a3168e753b56015288498a74bb2b084510f3d
Vu Diep <vdiep8@student.cccd.edu>
2019-10-31 05:10:09 +0000

    auto commit 10/31/19 05:10:09

--- h23/h23.cpp
+++ h23/h23.cpp
@@ -12,8 +12,43 @@ string STUDENT = "vdiep8"; // Add your Canvas/occ-email ID
 #include "h23.h"
 
 // Add your code here
-alternatingSum()
+int alternatingSum(const int a[] , size_t size)
+{
+    int sum = 0;
+    for(size_t i = 0; i < size ; i++)
+    {
+        if(i % 2 == 0) sum += a[i];
+        else sum-= a[i];
+    }
+    return sum;
+}
+MinMax minMax(const double *ptr, size_t size)
+{
 
+    struct MinMax result;
+
+    result.min = ptr;
+    result.max = ptr;
+
+    auto * atEnd = ptr + (size - 1);
+    if(size == 0)
+    {
+        return result;
+    }
+    while (ptr < atEnd)
+    {
+        ptr++;
+        if(*ptr > *(result.max))
+        {
+            (result.max) = ptr;
+        }
+        if(*ptr < *(result.min))
+        {
+            (result.min) = ptr;
+        }
+    }
+    return result;
+}
 /////////////// STUDENT TESTING ////////////////////
 
 int run()
