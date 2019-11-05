c1ab3f264f5c68723f5a9caf805f5849d6d7d084
Vu Diep <vdiep8@student.cccd.edu>
2019-11-05 08:03:22 +0000

    auto commit 11/05/19 08:03:20

--- h24/h24.cpp
+++ h24/h24.cpp
@@ -7,12 +7,48 @@
 #include <iostream>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "vdiep8"; // Add your Canvas/occ-email ID
 
 #include "h24.h"
 
 // Add your code here
+void translate(UC* const img, int width, int height, int dx, int dy)
+{
+    Pixel * pixel = reinterpret_cast<Pixel *>(img);
+    for(int row = 0; row < height ; row++)
+    {
+        if(dx < 0) dx = width - abs(dx) % width;
+        else dx = dx % width;
+        for(int j = 0 ; j < dx ; j++)
+        {
+            auto r = pixel + (row * width);
+            auto temp = r[width - 1];
+            for(int i = width - 1 ; i > 0; i--)
+            {
+                r[i] = r[i - 1];
+            }
+            r[0] = temp;
+        }
+        }
+        for(int i = 0 ; i < dy ; i++)
+        {
+             if(dy < 0) dy = height - abs(dx) % height;
+             else dy = dy % height;
 
+
+            for(int col = 0 ; col < width; col++)
+            {
+                auto r = pixel + width * (height - 1);
+                auto lastRow = r[col];
+                for(int row = height - 1 ; row > 0 ; row--)
+                pixel[(row * width) + col] = pixel[(row - 1) * width + col];
+                pixel[col] = lastRow;
+            }
+
+        }
+
+
+}
 
 
 /////////////// STUDENT TESTING ////////////////////
