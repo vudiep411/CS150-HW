807906b3bb0355a19abdebb3996e9b5f442ac94f
Vu Diep <vdiep8@student.cccd.edu>
2019-11-09 22:52:02 +0000

    auto commit 11/09/19 22:52:01

--- h26/h26.cpp
+++ h26/h26.cpp
@@ -14,52 +14,53 @@ string STUDENT = "vdiep8"; // Add your Canvas/occ-email ID
 // Add your code here
 void reverse(char * s)
 {
-char * a = s;
-char * b = s;
-while(*b)
-{
-    b++;
-    if(*b == '\0')
+    char * a = s;
+    char * b = s;
+
+    while (*b)
     {
-        b--;
-        break;
+        b++;
+        if (*b == '\0')
+        {
+            b--;
+            break;
+        }
     }
-}
- while(a < b)
+
+    while (a < b)
     {
-        char temp = *a;
+        auto temp = *a;
         *a = *b;
         *b = temp;
         a++;
         b--;
     }
-
 }
+
 const char * findStr(const char *str1, const char *str2)
 {
-    if (*str2 == '\0')  return str1;
-
-    size_t len = 0;
+    if (*str2 == '\0') { return str1; }
+    size_t len = -1;
     auto temp = *str1;
-    while (temp)  {len++ , temp++;}
+    while (temp != '\0') { len++; temp++; }
 
     for (size_t i = 0; i < len; i++)
     {
         auto p = str1 + i;
         auto p1 = str1 + i;
         auto p2 = str2;
-        while (*p1  && *p2  && *p1 == *p2)
+        while (*p1 != '\0' && *p2 != '\0' && *p1 == *p2)
         {
             p1++;
             p2++;
         }
-        if (*p2 == '\0')  return p;
-        else if(*p2) return 0;
+        if (*p2 == '\0') { return p; }
+        else if (*p1 == '\0') { return nullptr; }
     }
     return nullptr;
 }
 
-//////////////////////// STUDENT TESTING //////////////////////////
+
 int run()
 {
     cout << "Add your own tests here" << endl;
