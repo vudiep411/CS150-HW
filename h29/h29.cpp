6aac7f2dc855e86990de40deb2c48cb621da12f2
Vu Diep <vdiep8@student.cccd.edu>
2019-11-21 06:11:21 +0000

    auto commit 11/21/19 06:11:20

--- h29/h29.cpp
+++ h29/h29.cpp
@@ -8,13 +8,86 @@
 #include <stdexcept>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "vdiep8"; // Add your Canvas/occ-email ID
 #include "h29.h"
 
 Pixel GRAY{128, 128, 128, 128}; // a global gray for stubbing
 
 // Add your code here
-
+Image::Image(unsigned width, unsigned height)
+{
+    width_ = width;
+    height_ = height;
+    pixels_ = vector<Pixel>(width * height);
+}
+Image::Image(const string& path)
+{
+   load(path);
+}
+// void info(const string& msg, const Image& img)
+// {
+//     cout << msg << ": size()->" << img.size() << ", witdth()->" << img.width() << ", height()->" << img.height() << endl;
+// }
+ unsigned Image::width() const{
+     return width_;
+ }
+unsigned Image::height() const{
+     return  height_;
+}
+unsigned Image::size() const{
+    return pixels_.size();
+}
+void Image::fill(const Pixel& color)
+{
+     for(size_t i = 0 ; i < pixels_.size() ; i++)
+     {
+         pixels_[i] = color;
+     }
+}
+Pixel& Image::at(unsigned pos){
+    return pixels_.at(pos);
+}
+Pixel& Image::at(unsigned x, unsigned y){
+    return pixels_.at(width_ * y + x);
+}
+bool Image::load(const std::string& path)
+{
+    int width , height , bpp , channels = 4;
+    unsigned char * temp = stbi_load(path.c_str() , &width , &height , &bpp , channels );
+    if(temp != nullptr)
+    {
+        width_ = width;
+        height_ = height;
+        Pixel *p = reinterpret_cast<Pixel*>(temp);
+        pixels_.resize(width * height);
+        stbi_image_free(temp);
+        return true;
+    }
+    return false;
+}
+bool Image::save(const std::string& path)
+{
+ size_t dot = path.rfind(".");
+ string ex = path.substr(dot + 1);
+ if(dot == string::npos) return false;
+ for(auto &e : ex) e = tolower(e);
+  if(ex == "png")
+ {
+  stbi_write_png(path.c_str() , width_ , height_ , 4 , reinterpret_cast<unsigned char*>(pixels_.data()) , 4 * width_ );
+  return true;
+ }
+  else if(ex == "jpg")
+ {
+  stbi_write_jpg(path.c_str() , width_ , height_ , 4 , reinterpret_cast<unsigned char*>(pixels_.data()) , 100 );
+  return true;
+ }
+  else if(ex == "bmp")
+ {
+  stbi_write_bmp(path.c_str() , width_ , height_ , 4 , reinterpret_cast<unsigned char*>(pixels_.data()));
+  return true;
+ }
+ return false;
+}
 
 //////////////////////// STUDENT TESTING //////////////////////////
 void info(const string& msg, const Image& img);
