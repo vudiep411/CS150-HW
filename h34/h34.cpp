a3a0ca373f2eb9065b0994a6935a3d6aa66aa106
Vu Diep <vdiep8@student.cccd.edu>
2019-12-03 04:39:56 +0000

    auto commit 12/03/19 04:39:55

--- h34/h34.cpp
+++ h34/h34.cpp
@@ -14,7 +14,32 @@ using namespace std;
 string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
 
 ///////// Add your code here ///////////////
-
+Circle::Circle(double x, double y, double r) : Point(y, r) // parameters are mixed up.
+{
+    radius = x;
+}
+double Circle::getRadius() const { return radius; }
+double Circle::getArea() const { return PI * pow(getRadius(), 2); }
+double Circle::getCircumference() const { return PI * 2 * getRadius(); }
+string Circle::toString(int decimals) const
+{
+    ostringstream out;
+    out << setprecision(decimals) << fixed << "Circle(radius=" << getRadius() << ", center=" << Point::toString(decimals) << ")"; //Point(" << getX() << ", " << getY() << "))";
+    return out.str();
+}
+Cylinder::Cylinder(double h, double r, double x, double y) : Circle(r, x, y)
+{
+    height = h;
+}
+double Cylinder::getHeight() const { return height; }
+double Cylinder::getVolume() const { return Circle::getArea() * getHeight(); }
+double Cylinder::getArea() const { return (2 * PI * Circle::getRadius() * getHeight()) + (2 * Circle::getArea()); }
+string Cylinder::toString(int decimals) const
+{
+    ostringstream out;
+    out << setprecision(decimals) << fixed << "Cylinder(height=" << getHeight() << ", base=" << Circle::toString(decimals) << ")";
+    return out.str();
+}
 
 //////////////// Student Tests ////////////////////////////
 #include <iostream>
